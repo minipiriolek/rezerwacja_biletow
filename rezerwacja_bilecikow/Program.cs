a6586d330cdaffb2f6e50df5f2db45ef22393644
Twoje Imię <aleksander.malicki@cyfroweszkoly.pl>
2024-12-22 21:43:39 +0100

    dodawnie klasy concert oraz ticket

---
+++ rezerwacja_bilecikow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+class Concert
+{
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public int AvailableSeats { get; set; }
+
+    public Concert(string name, DateTime date, string location, int availableSeats)
+    {
+        Name = name;
+        Date = date;
+        Location = location;
+        AvailableSeats = availableSeats;
+    }
+}
+class Ticket
+{
+    public Concert Concert { get; set; }
+    public decimal Price { get; set; }
+    public int SeatNumber { get; set; }
+
+    public Ticket(Concert concert, decimal price, int seatNumber)
+    {
+        Concert = concert;
+        Price = price;
+        SeatNumber = seatNumber;
+    }
+}
